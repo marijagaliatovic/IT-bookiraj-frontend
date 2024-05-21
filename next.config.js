4811995a832f797f289e040e0ac1f57cc0c71e22
marijagaliatovic <92432581+marijagaliatovic@users.noreply.github.com>
2024-05-21 19:19:42 +0200

    changed express api url

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ const nextConfig = {
     },
     env: {
         CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-        CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
+        CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+        EXPRESS_API_URL: "https://backend-66n9.onrender.com"
       },
 };
 
