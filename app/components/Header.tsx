6193a9cb2adb2c59aff8f91fe43b581f4a1bbc5f
marijagaliatovic <92432581+marijagaliatovic@users.noreply.github.com>
2024-05-21 23:43:44 +0200

    changed back

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,6 +6,7 @@ import React, { useContext, useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function Header() {
+  localStorage.clear();
  // const { user, userInfo, renderFlag } = useContext(UserContext);
   const [isAccomodationOpen, setIsAccommodationOpen] = useState(false);
   const [newUser, setnewUser] = useState<string>("");
