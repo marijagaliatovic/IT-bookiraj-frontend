579c550e0b1242cc233b8f808103c601a7d0c258
marijagaliatovic <92432581+marijagaliatovic@users.noreply.github.com>
2024-05-25 12:33:05 +0200

    logging out

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -59,11 +59,11 @@ export default function Account(){
         fetchData();
     }, [userId]);
 
-    const handleLogout = async ()=> {
+    const handleLogout =  ()=> {
         localStorage.clear();
         setUser(undefined);
         //need to clear session on backend when user logs out
-         try{
+         /* try{
             const response = await fetch( `${process.env.EXPRESS_API_URL}/logout`, {
                 method: "POST",
                 credentials: "include"
@@ -76,7 +76,7 @@ export default function Account(){
         }catch(error){
             console.error('Error logging out:', error);
         } 
-        
+         */
     }
 
     return (
