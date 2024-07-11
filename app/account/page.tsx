207be04e423e1a0774a2cb5c5ea2b80688e8b6fe
karladelic <kdelic01@fesb.hr>
2024-07-11 23:14:39 +0200

    added profile pic

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 import { useEffect, useState } from "react";
+import blankprofilepic from "@/public/images/blankprofilepic.jpg";
+import Image from "@/node_modules/next/image";
 
 export interface User{
     email:string,
@@ -79,26 +81,33 @@ export default function Account(){
     }
 
     return (
-        <>
-            <NavBar/>
-            <div className="flex flex-col items-center relative lg:mt-12 py-8">
-                <h2 className="font-bold text-xl lg:text-2xl self-center lg:mt-10 top-24">User</h2>
-                <div className="small-line"></div>
+        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col items-center relative lg:mt-12 py-8 w-full lg:max-content flex-grow ">
+          <h2 className="font-bold text-xl lg:text-2xl self-center lg:mt-10 top-24">
+            Account
+          </h2>
+          <div className="small-line"></div>
+          <div className="bg-white mb-10 mt-10 pt-10 pb-10 mx-5 sm:mx-auto md:mx-auto max-w-md px-10 rounded-md shadow-lg">
+            <div className=" bg-white w-36 h-36">
+            <Image src={blankprofilepic} width={1000} height={400} alt="slika" className="rounded-full" />
             </div>
-            
-            <div className="bg-white mb-20 pb-10 mx-5 sm:mx-auto md:mx-auto max-w-md px-10 rounded-md shadow-lg">
-                <label className="bg-white block mb-2 text-sm pt-10 font-semibold">Email: {user?.email}</label>
-                <label className="bg-white block mt-4 mb-2 text-sm font-semibold">Username: {user?.userName}</label>
-                <div className="text-center bg-white">
-                    <Link href="\">
-                        <button onClick={handleLogout} className="mt-5 bg-gray-400 font-bold p-2 rounded-md hover:bg-slate-600 shadow-xl">
-                            Log out
-                        </button>
-                    </Link>
-                </div>
-            </div>
-            
-            <Footer />
-        </>
-    );
+            <label className="bg-white block mb-2 mt-10 text-sm font-semibold">
+            Email:  <a className="font-light bg-white ">{user?.email}</a>
+            </label>
+            <label className="bg-white block mt-4 mb-2 text-sm font-semibold">
+            Username: <a className="font-light bg-white ">{user?.userName}</a> 
+            </label>
+            <div className="text-center bg-white">
+                     <Link href="\">
+                     <button onClick={handleLogout} className="mt-5 bg-gray-400 font-bold p-2 rounded-md hover:bg-slate-600 shadow-xl">
+                                Log out
+                            </button>
+                     </Link>
+                 </div>
+          </div>  
+        </div>
+        <Footer/>
+    </div>
+        
+      );
 }
