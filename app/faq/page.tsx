f9af68189871ea1fc76726445d6617d4bdc4f668
karladelic <kdelic01@fesb.hr>
2024-07-12 00:45:12 +0200

    reduced number of questions

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -46,19 +46,20 @@ export default function Faq() {
   return (
     <>
     <NavBar/>
-      <div className="flex flex-col items-center relative lg:mt-12 py-8">
+    <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col items-center relative lg:mt-12 py-8 w-full lg:max-content flex-grow">
         <h2 className="font-bold text-xl lg:text-2xl self-center lg:mt-10 top-24">
           Frequently asked questions
         </h2>
         <div className="small-line"></div>
       </div>
 
-      <div className="bg-white mb-20 pt-5 pb-10 sm:mx-auto md:mx-auto max-w-md px-10 rounded-md shadow-lg mt-5 lg:max-content">
+      <div className="bg-white mb-10 pb-10 mx-10 sm:mx-auto md:mx-auto max-w-md px-12 rounded-md shadow-lg sm:w-2/3 w-4/5">
         <button
-          className="bg-white text-left font-bold mt-10 flex flex-row"
+          className="bg-white text-left font-bold mt-10 flex gap-3 flex-row"
           onClick={toogleAnswer}
         >
-          What is the check-in/check-out time for the apartment?{" "}
+          What is the check-in/check-out time?{" "}
           <svg
             height="26"
             viewBox="0 0 48 48"
@@ -81,10 +82,10 @@ export default function Faq() {
         )}
         <hr className="mt-10"></hr>
         <button
-          className="bg-white text-left font-bold mt-10 flex flex-row"
+          className="bg-white text-left font-bold mt-10 flex gap-3 flex-row"
           onClick={toogleAnswer2}
         >
-          Is there a security deposit required when booking an apartment?{" "}
+          What are the cancellation policies?{" "}
           <svg
             height="26"
             viewBox="0 0 48 48"
@@ -100,10 +101,7 @@ export default function Faq() {
         </button>
         {openAnswer2 && (
           <p className="bg-white mt-5">
-            Many apartments require a security deposit to cover any damages or
-            incidents during your stay. The amount varies depending on the
-            property and is usually refunded after check-out if no damage is
-            found.
+           Cancellation policies vary between apartments and booking platforms. Some may offer free cancellation up to a certain date, while others might impose penalties for late cancellations or no-shows. Before making a reservation, it&apos;s crucial to review and understand the specific cancellation terms..
           </p>
         )}
         <hr className="mt-10"></hr>
@@ -111,8 +109,7 @@ export default function Faq() {
           className="bg-white text-left font-bold mt-10 flex flex-row"
           onClick={toogleAnswer3}
         >
-          Are utilities such as electricity, water, and internet included in the
-          rental price?{" "}
+          How do I handle maintenance issues during my stay{" "}
           <svg
             height="26"
             viewBox="0 0 48 48"
@@ -128,9 +125,7 @@ export default function Faq() {
         </button>
         {openAnswer3 && (
           <p className="bg-white mt-5">
-            This varies between apartments. Some include utilities in the rental
-            price, while others may charge them separately. It&apos;s essential to
-            clarify this with the property owner or manager before booking.
+            Each apartment may have different procedures for addressing maintenance concerns. Typically, you can contact the property manager or maintenance team directly for urgent issues. It&apos;s helpful to have their contact details readily available for quick assistance.
           </p>
         )}
         <hr className="mt-10"></hr>
@@ -159,91 +154,11 @@ export default function Faq() {
             facilities. Some apartments may offer additional amenities like gym
             access, parking, or a pool.
           </p>
-        )}
-        <hr className="mt-10"></hr>
-        <button
-          className="bg-white text-left font-bold mt-10 flex flex-row"
-          onClick={toogleAnswer5}
-        >
-          Is there a minimum stay requirement for booking an apartment?{" "}
-          <svg
-            height="26"
-            viewBox="0 0 48 48"
-            width="26"
-            xmlns="http://www.w3.org/2000/svg"
-            className={` bg-transparent ${
-              openAnswer5 ? "rotate-180" : "rotate-0"
-            }`}
-          >
-            <path d="M33.17 17.17l-9.17 9.17-9.17-9.17-2.83 2.83 12 12 12-12z" />
-            <path d="M0 0h48v48h-48z" fill="none" />
-          </svg>
-        </button>
-        {openAnswer5 && (
-          <p className="bg-white mt-5">
-            Many apartments have a minimum stay requirement, which can range
-            from one night to a week or more. It&apos;s essential to check the
-            property&apos;s booking terms and conditions for this information.
-          </p>
-        )}
-        <hr className="mt-10"></hr>
-        <button
-          className="bg-white text-left font-bold mt-10 flex flex-row"
-          onClick={toogleAnswer6}
-        >
-          Can I cancel or modify my reservation, and what are the associated
-          fees?{" "}
-          <svg
-            height="26"
-            viewBox="0 0 48 48"
-            width="26"
-            xmlns="http://www.w3.org/2000/svg"
-            className={` bg-transparent ${
-              openAnswer6 ? "rotate-180" : "rotate-0"
-            }`}
-          >
-            <path d="M33.17 17.17l-9.17 9.17-9.17-9.17-2.83 2.83 12 12 12-12z" />
-            <path d="M0 0h48v48h-48z" fill="none" />
-          </svg>
-        </button>
-        {openAnswer6 && (
-          <p className="bg-white mt-5">
-            The cancellation and modification policies vary between properties
-            and booking platforms. Some offer free cancellation within a certain
-            period, while others may charge fees. Always review the cancellation
-            policy before booking.
-          </p>
-        )}
-        <hr className="mt-10"></hr>
-        <button
-          className="bg-white text-left font-bold mt-10 flex flex-row"
-          onClick={toogleAnswer7}
-        >
-          How do I collect the keys and access the apartment?{" "}
-          <svg
-            height="26"
-            viewBox="0 0 48 48"
-            width="26"
-            xmlns="http://www.w3.org/2000/svg"
-            className={` bg-transparent ${
-              openAnswer7 ? "rotate-180" : "rotate-0"
-            }`}
-          >
-            <path d="M33.17 17.17l-9.17 9.17-9.17-9.17-2.83 2.83 12 12 12-12z" />
-            <path d="M0 0h48v48h-48z" fill="none" />
-          </svg>
-        </button>
-        {openAnswer7 && (
-          <p className="bg-white mt-5">
-            The process for collecting keys and accessing the apartment varies.
-            Some properties may have a key lockbox, while others require meeting
-            with a property manager. Clear instructions are usually provided
-            upon booking.
-          </p>
-        )}
+        )}        
       </div>
 
       <Footer />
+      </div>
     </>
   );
 }
