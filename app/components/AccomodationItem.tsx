9ed08c5ff3e35e51e68e8a74d204cc1bcf24f6a7
karladelic <kdelic01@fesb.hr>
2024-07-12 00:30:14 +0200

    added link on card

--- app/components/AccomodationItem.tsx
+++ app/components/AccomodationItem.tsx
@@ -2,8 +2,6 @@
 
 import Image from "next/image";
 import Link from "@/node_modules/next/link";
-import Apartment from "../apartmentlistings/[apartmentId]/page";
-import contentfulService, { apartmentsItem } from "@/lib/.contentfulClient";
 
 
 export interface images {
@@ -32,42 +30,37 @@ export interface accomodation {
 
 const AccomodationItem = (item: accomodation) => {
   return (
-    <div className="top-0 w-full xl:w-1/4 lg:w-1/3 ml:w-1/4 rounded-md md:w-1/2  bg-white flex flex-col justify-around items-center lg:m-4 my-4 hover:shadow-2xl">
-      <div className="bg-white relative mt-6 lg:w-4/5 w-3/4  md:h-44 h-36 lg:h-44">
-        <Image fill={true} src={item.picture.url} alt={item.picture.title} />
+    <Link href={`/apartmentlistings/${item.apartmentId}`} className=" top-0 w-full xl:w-1/4 lg:w-1/3 ml:w-1/4 rounded-md md:w-1/2 bg-white flex flex-col justify-around items-center lg:m-4 my-4 hover:shadow-2xl">
+  <div className="bg-white relative mt-6 lg:w-4/5 w-3/4 md:h-44 h-36 lg:h-44">
+    <Image fill={true} src={item.picture.url} alt={item.picture.title} />
+  </div>
+  <div className="bg-white relative basis-6/12 w-8/12 m-4 flex flex-col justify-between gap-1 text-center">
+    <h3 key={item.apartmentId} className="bg-white self-center text-2xl font-bold mb-4 cursor-pointer font-serif not-italic tracking-widest">
+      {item.title}
+    </h3>
+    <div className="flex flex-col self-center justify-center m-3">
+      <div className="bg-white flex justify-center gap-1 items-end">
+        <p className="bg-white font-serif font-medium not-italic tracking-wide">
+          {item.location}
+        </p>
       </div>
-      <div className="bg-white relative basis-6/12 w-8/12 m-4 flex flex-col justify-between gap-1 text-center ">
-        <h3 key={item.apartmentId} className="bg-white self-center text-2xl font-bold mb-4 cursor-pointer font-serif not-italic tracking-widest">
-          <Link href={`/apartmentlistings/` + item.apartmentId}>{item.title}</Link>
-        </h3>
-        <div className="flex flex-col self-center justify-center m-3">
-          <div className="bg-white flex justify-center gap-1 items-end">
-            <p className="bg-white font-serif font-medium not-italic tracking-wide">
-              {item.location}
-            </p>
-          </div>
-          <div className="flex flex-col text-center bg-white">
-            <p className="bg-white self-center font-serif font-medium not-italic tracking-wide">
-              {item.size}
-            </p>
-            <p
-              className={` bg-white self-center font-serif font-medium not-italic tracking-wide ${
-                item.specialOffer !== 0 ? "line-through" : ""
-              }`}
-            >
-              {item.priceNumber}€ per night
-            </p>
-            {item.specialOffer !== 0 ? (
-              <p className="bg-white self-center font-serif font-bold not-italic tracking-wide">
-                {item.specialOffer}€ per night
-              </p>
-            ) : (
-              <> </>
-            )}
-          </div>
-        </div>
+      <div className="flex flex-col text-center bg-white">
+        <p className="bg-white self-center font-serif font-medium not-italic tracking-wide">
+          {item.size}
+        </p>
+        <p className={`bg-white self-center font-serif font-medium not-italic tracking-wide ${item.specialOffer !== 0 ? "line-through" : ""}`}>
+          {item.priceNumber}€ per night
+        </p>
+        {item.specialOffer !== 0 && (
+          <p className="bg-white self-center font-serif font-bold not-italic tracking-wide">
+            {item.specialOffer}€ per night
+          </p>
+        )}
       </div>
     </div>
+  </div>
+</Link>
+
   );
 };
 
