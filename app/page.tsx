d1284beacf7a9ebf567f7233362898060077bab3
karladelic <kdelic01@fesb.hr>
2024-07-12 01:14:42 +0200

    sticked footer to the bottom

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,21 @@ import WhatWeOffer from "./components/WhatWeOffer";
 import SpecialOffers from "./components/SpecialOffers";
 import Reviews from "./components/Reviews";
 import Footer from "./components/Footer";
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
 
 
 export default function Home() {
 
   return (
-    <div className=" relative top-0 lg:top-24 flex flex-col overflow-hidden">
+    <div className=" relative top-0 lg:top-24 flex flex-col min-h-screen overflow-hidden">
+      <div className="w-full lg:max-content flex-grow">
       <NavBar/>
       <HeroSection/>
       <WhatWeOffer/>
       <SpecialOffers/>
       <Reviews/>
       <Footer/>
+      </div>
     </div>
   )
 }
